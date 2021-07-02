b15e8f1a0a5f8f3d6ff1d2cdc7de84c31e8578c4
Kent Clark <kent@bitletters.link>
2021-07-01 19:45:38 -0700

    Enable running the install.sh from REPO_ROOT

--- macos/install.sh
+++ macos/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+cd ..
 
+cp ./macos/setup.py .
 python3 setup.py py2app -A
+rm setup.py
--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+
 pip3 install python-kasa --pre
 pyinstaller --copy-metadata=python-kasa -F ../main.py
 cp private_home.desktop ~/.local/share/applications
